ebbc921b91864e464ba024d1fe630b412ea513ae
marcuspc <marco_21_06@hotmail.com>
2021-03-10 17:57:30 -0500

    asig

--- views/mainJs/mainJsAsignacionFirmas.js
+++ views/mainJs/mainJsAsignacionFirmas.js
@@ -1,9 +1,70 @@
-$(window).on('load', function() { // makes sure the whole site is loaded
-	hidePreloader();
-});
+var form = '#formAsigFirma';
+var modalReg = '#modalReg';
+var modalLista = '#modalLista';
 
 init.push(function () {
 
-    //aqui tu codigo
+   var table = $('.tablaAsignacionFirma').DataTable( {
+      "ajax": {
+          url:"ajax/datatable-asignacionFirma.ajax.php",
+          data: function(d){
+              d.mantenimiento = $('.mantenimiento').val(); //enviar parametros personalizados
+          },
+          complete: function(res){
+              //console.log(res);
+              hidePreloader();
+              unBlockPage();
+          }
+      },
+      "deferRender": true,
+      "retrieve": true,
+      "processing": true,
+      "serverSide":true,
+      columns: [
+
+          {data: 'DT_RowIndex', name: 'DT_RowIndex',className:'text-center'},
+          {data: 'descripcion', name: 'descripcion',className:'text-center'},
+          {data: 'action', name: 'action', className:'text-center',orderable: false, searchable: false},
+
+      ],
+      "language": {
+
+          "sProcessing":     "Procesando...",
+          "sLengthMenu":     "Por Pagina _MENU_",
+          "sZeroRecords":    "No se encontraron resultados",
+          "sEmptyTable":     "Ningún dato disponible en esta tabla",
+          "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+          "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0",
+          "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+          "sInfoPostFix":    "",
+          "sSearch":         "",
+          "sUrl":            "",
+          "sInfoThousands":  ",",
+          "sLoadingRecords": "Cargando...",
+          "oPaginate": {
+          "sFirst":    "Primero",
+          "sLast":     "Último",
+          "sNext":     "Siguiente",
+          "sPrevious": "Anterior"
+          },
+          "oAria": {
+            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+          }
+
+      }
+
+    });
+
+    $('#DataTables_Table_0_wrapper .table-caption').text('Asignacion de Firma');
+  	$('#DataTables_Table_0_wrapper .dataTables_filter input').attr('placeholder', 'Buscar...');
+
+    $('.btn-registrar').click(function(){
+      $(modalReg+' #myModalLabel').text('Asignar Firma');
+      resetFormularioNew(form);
+      $(form+' input[name=id]').val('0');
+      $(modalReg).modal('show');
+
+    });
     
 });
--- views/modules/Asignacion_De_Firmas/asignacion_de_firmas.php
+++ views/modules/Asignacion_De_Firmas/asignacion_de_firmas.php
@@ -28,7 +28,24 @@ $mantenimiento = $enlaces->mantenimientoDatosController();
 
         <div class="panel-body">
             
-            Aqui tu codigo
+            <div class="form-group">
+                <button type="button" class="btn btn-primary btn-registrar">Asignar Firma</button>
+            </div>
+
+            <div class="table-primary">
+                <table class="table table-default table-condensed table-bordered table-striped tablaAsignacionFirma" width="100%">
+                    <thead>
+                        <tr>
+                            <th style="width:5% !important">#</th>
+                            <th style="width:30% !important">Usuario</th>
+                            <th style="width:20% !important">Logo</th>
+                            <th style="width:20% !important">Certificado</th>
+                            <th style="width:25% !important"></th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
 
         </div>
 
@@ -36,6 +53,62 @@ $mantenimiento = $enlaces->mantenimientoDatosController();
 
 </div> <!-- / #content-wrapper -->
 
+<!-- MODAL ASIGNAR FIRMA -->
+<div id="modalReg" class="modal fade" tabindex="-1" role="dialog" style="display: none;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body">
+                <form id="formAsigFirma" class="form">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label class="control-label">Usuarios</label>
+                                        <select name="usuario" id="usuario" class="form-control" required>
+                                            
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label class="control-label">Clave</label>
+                                        <input type="text" name="clave" value="clave" class="form-control" placeholder="Ingrese la clave">                                    
+                                    </div>
+                                </div>
+                            </div>
+                             <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label class="control-label">Imagen</label>
+                                        <input id="imagen" name="imagen" type="file" multiple="" />
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label class="control-label">Certificado</label>
+                                        <input id="certificado" name="certificado" type="file" multiple="" />                                  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                    <input type="hidden" name="id" id="id" class="form-control" value="0" required>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" id="guardar" class="btn btn-primary">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php
